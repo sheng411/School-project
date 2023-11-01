993b35135d1e43f313f4ba9813ad15aa05211e40
sheng411 <jerry65855@gmail.com>
2023-11-02 00:30:35 +0800

    added reception GUI and improved some progreams

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from tkinter import *
-from tkinter import ttk
-from tkinter import filedialog
+from tkinter import ttk,filedialog
+from photo import *
 
 window=Tk() #start
 
@@ -11,12 +11,15 @@ window.title('AES')        # title
 frame_1=Frame(window)
 frame_1.pack()
 
+
 '''     send GUI    '''
+
 def send_submit():
     data_text=send_gui_input.get()
     print("Send input-->",data_text)
 
 def file_show():
+    global file_path
     file_path=filedialog.askopenfilename()
     print(file_path)
 
@@ -53,14 +56,41 @@ send_gui_button.grid(row=1,column=4)
 
 
 for widget in send_gui.winfo_children():        #spacing
-    widget.grid_configure(padx=10,pady=5)
-
+    widget.grid_configure(padx=5,pady=5)
 
 
 '''     reception GUI   '''
+
+def photo_show():
+    if file_path:
+        open_photo(f'{file_path}')
+    else:
+        print("Oh NO")
+
+reception_gui=LabelFrame(frame_1,text="Reception")
+reception_gui.grid(row=1,column=0)
+
+reception_gui_prompts=Label(reception_gui,text="Enter text")    #input prompts
+reception_gui_prompts.grid(row=0,column=0)
+
+#text out area
+reception_gui_input=Entry(reception_gui)      #output area
+reception_gui_input.grid(row=1,column=0)
+
+reception_gui_text_out=Label(reception_gui,text="    ")
+reception_gui_text_out.grid(row=1,column=1)
+
+#photo and video area
+reception_gui_button=Button(reception_gui,text="Show",command=photo_show,height=3,width=10)
+reception_gui_button.grid(row=1,column=2)
+
+
+for widget in reception_gui.winfo_children():        #spacing
+    widget.grid_configure(padx=5,pady=5)
 
 
 '''     other   '''
 
 window.geometry("600x400")  #window size
+window.resizable(True,True)   # disable page size cheng
 window.mainloop()   #end
