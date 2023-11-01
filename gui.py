6949ac926dfec2b4f44d32f87d2351ac94b0b111
sheng411 <jerry65855@gmail.com>
2023-11-02 02:08:37 +0800

    added file size and photo path check function

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
-from tkinter import ttk,filedialog
+from tkinter import ttk,filedialog,messagebox
 from photo import *
+import os
 
 window=Tk() #start
 
@@ -11,6 +12,7 @@ window.title('AES')        # title
 frame_1=Frame(window)
 frame_1.pack()
 
+file_path=''
 
 '''     send GUI    '''
 
@@ -22,6 +24,17 @@ def file_show():
     global file_path
     file_path=filedialog.askopenfilename()
     print(file_path)
+    file_size()
+
+def file_size():        #get sile size
+    global fs_check
+    fs_ckeck=0
+    file_size=os.path.getsize(file_path) #get file path
+    fs_kb=file_size/1024    #conversion kb
+    print(f"File size: {file_size} bytes ({fs_kb:.2f} KB)")
+    if fs_kb>129:
+        fs_ckeck=1
+
 
 
 send_gui=LabelFrame(frame_1,text="Send")
@@ -62,15 +75,16 @@ for widget in send_gui.winfo_children():        #spacing
 '''     reception GUI   '''
 
 def photo_show():
-    if file_path:
+    if file_path!='':
         open_photo(f'{file_path}')
     else:
         print("Oh NO")
+        messagebox.showwarning('Warning','No path!!!')
 
 reception_gui=LabelFrame(frame_1,text="Reception")
 reception_gui.grid(row=1,column=0)
 
-reception_gui_prompts=Label(reception_gui,text="Enter text")    #input prompts
+reception_gui_prompts=Label(reception_gui,text="Private text")    #input prompts
 reception_gui_prompts.grid(row=0,column=0)
 
 #text out area
@@ -81,7 +95,7 @@ reception_gui_text_out=Label(reception_gui,text="    ")
 reception_gui_text_out.grid(row=1,column=1)
 
 #photo and video area
-reception_gui_button=Button(reception_gui,text="Show",command=photo_show,height=3,width=10)
+reception_gui_button=Button(reception_gui,text="Show",command=photo_show,height=2,width=10)
 reception_gui_button.grid(row=1,column=2)
 
 
