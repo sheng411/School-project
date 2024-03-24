4911a90a5f39e056daed8f9c1db5915d1d3bc3d8
sheng411 <jerry65855@gmail.com>
2024-03-25 02:25:46 +0800

    fix send program(data variable add "\n")

--- serial_sensor.py
+++ serial_sensor.py
@@ -1,11 +1,12 @@
 import serial
 
-ser = serial.Serial('COM9', 115200)
+ser = serial.Serial('COM6', 115200)
 
 
 def serial_send_data(data):
     try:
         print(f"Sending data: {data}")
+        data+="\n"
         ser.write(data.encode('UTF-8'))  # Encode data to bytes for serial transmission
 
     except serial.SerialException as e:
@@ -18,21 +19,17 @@ def serial_listen_function():
     for i in range(1):
         # get data and decode
         data = ser.readline().decode('UTF-8').rstrip()
-        # show data
-        #print(data)
+
         # check the keyword "close"
-        '''
-        if data.lower() == "close":
-            print("received 'close' ,Shutting down the serial port....")
-            break
-        '''
         return data
 
 
 ##test
 '''
 while True:
-    d=input("-->")
-    print(serial_send_data(d))
     
-'''
+    d=input("-->")
+    print("p->",serial_send_data(d))
+    
+    print(serial_listen_function())
+    '''
